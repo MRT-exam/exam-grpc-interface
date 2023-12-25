1a39888783ae277d388158734101da303378f929
RasmusDalgaard <cph-rd92@cphbusiness.dk>
2023-12-25 15:06:35 +0100

    first commit

---
+++ src/main/java/com/mtgo/exam/grpcinterface/ExamGrpcInterfaceApplication.java
@@ -0,0 +1,13 @@
+package com.mtgo.exam.grpcinterface;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamGrpcInterfaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamGrpcInterfaceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mtgo/exam/grpcinterface/ExamGrpcInterfaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mtgo.exam.grpcinterface;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamGrpcInterfaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
